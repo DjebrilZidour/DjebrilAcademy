f4c62febf040d365f71825df5bbc5b4aeb074ce6
Djebril <djebrilacademy@gmail.com>
2023-07-17 00:40:57 +0100

    not stysfing ui at all

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,33 +1,46 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
+const Singup = (props) => {};
+
 const Login = (props) => {
   const [typedEmail, setTypedEmail] = useState("");
   const [typedPassword, setTypedPassword] = useState("");
   return (
-    <div className="">
-      <h1>Log in</h1>
-      <input className="border-2 border-black"
-        value={typedEmail}
-        placeholder="e-mail"
-        type="email"
-        onChange={(e) => {
-          setTypedEmail(e.target.value);
-        }}
-      />
-      <input value={typedPassword} className="border-2 border-black" placeholder="password" type="password" onChange={(e)=>{
-        setTypedPassword(e.target.value)
-      }}/>
-      <button
-        className="border-2 border-black"
-        onClick={() => {
-          setTypedEmail(typedEmail);
-          setTypedPassword(typedPassword)
-          console.log(typedEmail);
-          console.log(typedPassword);
-        }}
-      >
-        Log in
-      </button>
+    <div className="bg-amber-300 w-screen h-screen w-screen h-screen flex justify-center items-center ">
+      <div className="  flex justify-center gap-4 flex-col items-center border-2 border-black p-4 rounded-2xl">
+        <h1 className="text-3xl text-white">Log in</h1>
+        <input
+        style={{border: "px solid #f19c12"}}
+          className=" p-4"
+          value={typedEmail}
+          placeholder="e-mail"
+          type="email"
+          onChange={(e) => {
+            setTypedEmail(e.target.value);
+          }}
+        />
+        <input
+        
+          value={typedPassword}
+          className="border-2 border-black p-4"
+          placeholder="password"
+          type="password"
+          onChange={(e) => {
+            setTypedPassword(e.target.value);
+          }}
+        />
+        <button
+          className="border-4 border-orange-50 px-8 py-2 rounded-2xl"
+          onClick={() => {
+            setTypedEmail(typedEmail);
+            setTypedPassword(typedPassword);
+            console.log(typedEmail);
+            console.log(typedPassword);
+          }}
+        >
+          Log in
+        </button>
+      </div>
     </div>
   );
 };
@@ -35,9 +48,9 @@ const Login = (props) => {
 function App() {
   return (
     <section id="app">
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Login /> */}
-      <header>
+      {/* <header>
         <div>
           <h1>Remember that your success is between your hands !!! </h1>
           <p>
@@ -55,7 +68,7 @@ function App() {
             alt=""
           />
         </div>
-      </header>
+      </header> */}
       <main>
         <Login />
       </main>
