2a85d38c8bd6c1df96782da4b0d9c47664eff776
Djebril <djebrilacademy@gmail.com>
2023-07-17 00:11:30 +0100

    finish the nav + log in form just code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,50 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
+const Login = (props) => {
+  const [typedEmail, setTypedEmail] = useState("");
+  const [typedPassword, setTypedPassword] = useState("");
+  return (
+    <div className="">
+      <h1>Log in</h1>
+      <input className="border-2 border-black"
+        value={typedEmail}
+        placeholder="e-mail"
+        type="email"
+        onChange={(e) => {
+          setTypedEmail(e.target.value);
+        }}
+      />
+      <input value={typedPassword} className="border-2 border-black" placeholder="password" type="password" onChange={(e)=>{
+        setTypedPassword(e.target.value)
+      }}/>
+      <button
+        className="border-2 border-black"
+        onClick={() => {
+          setTypedEmail(typedEmail);
+          setTypedPassword(typedPassword)
+          console.log(typedEmail);
+          console.log(typedPassword);
+        }}
+      >
+        Log in
+      </button>
+    </div>
+  );
+};
 
 function App() {
-  console.log("hi");
   return (
     <section id="app">
       <Navbar />
+      {/* <Login /> */}
       <header>
-
         <div>
           <h1>Remember that your success is between your hands !!! </h1>
           <p>
-            Unlock boundless knowledge with our innovative educational
-            platform. Learn, grow, and thrive with interactive courses, engaging
-            videos, and personalized learning pathways. Fuel your curiosity and
-            unleash your potential today!
+            Unlock boundless knowledge with our innovative educational platform.
+            Learn, grow, and thrive with interactive courses, engaging videos,
+            and personalized learning pathways. Fuel your curiosity and unleash
+            your potential today!
           </p>
           <button>start learning now</button>
         </div>
@@ -24,12 +55,10 @@ function App() {
             alt=""
           />
         </div>
-
       </header>
       <main>
-        
+        <Login />
       </main>
-
     </section>
   );
 }
