8601c15b8ae1bcd593a3dc0fd00afaa908440b52
Djebril <djebrilacademy@gmail.com>
2023-07-16 23:17:08 +0100

    navbar done html + css

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,36 @@
+import Navbar from "../components/Navbar";
+
 function App() {
-  
+  console.log("hi");
   return (
-    <>
-      <div>welcome</div>
-    </>
+    <section id="app">
+      <Navbar />
+      <header>
+
+        <div>
+          <h1>Remember that your success is between your hands !!! </h1>
+          <p>
+            Unlock boundless knowledge with our innovative educational
+            platform. Learn, grow, and thrive with interactive courses, engaging
+            videos, and personalized learning pathways. Fuel your curiosity and
+            unleash your potential today!
+          </p>
+          <button>start learning now</button>
+        </div>
+
+        <div>
+          <img
+            src="https://images.pexels.com/photos/4195504/pexels-photo-4195504.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt=""
+          />
+        </div>
+
+      </header>
+      <main>
+        
+      </main>
+
+    </section>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 const Navbar = () => {
   return (
-    <>
-      <nav>
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-      </nav>
-    </>
+    <nav className="bg-amber-400 px-16 py-2 rounded-bl-3xl ">
+      <ul className="list-none flex justify-between items-center">
+        <li>
+          <h1 className="cursor-pointer text-2xl">LOGO</h1>
+        </li>
+
+        <div className="flex justify-between items-center text-white">
+
+          <li className="px-4 hover:text-black cursor-pointer">home</li>
+          <li className="px-4 hover:text-black cursor-pointer">about us</li>
+          <li className="px-4 hover:text-black cursor-pointer">start learning</li>
+
+          <div className="flex justify-between items-center ml-4">
+            <button className="border-2 border-white rounded-2xl py-1 px-4 mx-2 hover:bg-white hover:text-amber-400">Log in</button>
+            <button className="border-2 border-white rounded-2xl py-1 px-4 hover:bg-white hover:text-amber-400">Sing up</button>
+          </div>
+
+        </div>
+
+      </ul>
+    </nav>
   );
 };
+export default Navbar;
