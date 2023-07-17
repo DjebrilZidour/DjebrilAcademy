f7e420b023341c834d66d6eca98b31def0fbe743
Abderahmane Aoufi <Abdouou7@gmail.com>
2023-07-17 06:12:32 +0100

    add login page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,72 +1,12 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
-const Singup = (props) => {};
-
-const Login = (props) => {
-  const [typedEmail, setTypedEmail] = useState("");
-  const [typedPassword, setTypedPassword] = useState("");
-  return (
-    <div className="bg-amber-300 w-screen h-screen w-screen h-screen flex justify-center items-center ">
-      <div className="  flex justify-center gap-4 flex-col items-center border-2 border-black p-4 rounded-2xl">
-        <h1 className="text-3xl text-white">Log in</h1>
-        <input
-          className=" border-2 border-black p-4"
-          value={typedEmail}
-          placeholder="e-mail"
-          type="email"
-          onChange={(e) => {
-            setTypedEmail(e.target.value);
-          }}
-        />
-        <input
-          value={typedPassword}
-          className="border-2 border-black p-4"
-          placeholder="password"
-          type="password"
-          onChange={(e) => {
-            setTypedPassword(e.target.value);
-          }}
-        />
-        <button
-          className="border-4 border-orange-50 px-8 py-2 rounded-2xl"
-          onClick={() => {
-            setTypedEmail(typedEmail);
-            setTypedPassword(typedPassword);
-            console.log(typedEmail);
-            console.log(typedPassword);
-          }}
-        >
-          Log in
-        </button>
-      </div>
-    </div>
-  );
-};
+import Login from "../pages/auth/Login/Login";
 
 function App() {
   return (
     <section id="app">
       <Navbar />
-      <header className="flex justify-between p-4 ">
-        <div>
-          <h1>Remember that your success is between your hands !!! </h1>
-          <p>
-            Unlock boundless knowledge with our innovative educational platform.
-            Learn, grow, and thrive with interactive courses, engaging videos,
-            and personalized learning pathways. Fuel your curiosity and unleash
-            your potential today!
-          </p>
-          <button>start learning now</button>
-        </div>
-
-        <div>
-          <img
-            src="https://images.pexels.com/photos/4195504/pexels-photo-4195504.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-          />
-        </div>
-      </header>
-      <main>{/* <Login /> */}</main>
+      <Login />
     </section>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,29 @@
+import Logo from "../assets/images/logo.png";
+
 const Navbar = () => {
   return (
-    <nav className="bg-amber-400 px-16 py-2 rounded-bl-3xl ">
+    <nav className="bg-amber-50 shadow-lg px-16 py-2 rounded-bl-3xl  ">
       <ul className="list-none flex justify-between items-center">
         <li>
-          <h1 className="cursor-pointer text-2xl">LOGO</h1>
+          <img className="h-12 w-12" src={Logo} />
         </li>
 
-        <div className="flex justify-between items-center text-white">
-
+        <div className="flex justify-between items-center text-black-100">
           <li className="px-4 hover:text-black cursor-pointer">home</li>
           <li className="px-4 hover:text-black cursor-pointer">about us</li>
-          <li className="px-4 hover:text-black cursor-pointer">start learning</li>
+          <li className="px-4 hover:text-black cursor-pointer">
+            start learning
+          </li>
 
           <div className="flex justify-between items-center ml-4">
-            <button className="border-2 border-white rounded-2xl py-1 px-4 mx-2 hover:bg-white hover:text-amber-400">Log in</button>
-            <button className="border-2 border-white rounded-2xl py-1 px-4 hover:bg-white hover:text-amber-400">Sing up</button>
+            <button className="border-2 border-white rounded-2xl py-1 px-4 mx-2 hover:bg-white hover:text-amber-400">
+              Log in
+            </button>
+            <button className="border-2 border-white rounded-2xl py-1 px-4 hover:bg-white hover:text-amber-400">
+              Sing up
+            </button>
           </div>
-
         </div>
-
       </ul>
     </nav>
   );
