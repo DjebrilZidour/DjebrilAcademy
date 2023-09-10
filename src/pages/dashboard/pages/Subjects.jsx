72853efd8fa23afb8b364d57fb0b41b426cb6204
Djebril <djebrilacademy@gmail.com>
2023-09-10 22:59:25 +0100

    fix matiers

--- src/pages/dashboard/pages/Subjects.jsx
+++ src/pages/dashboard/pages/Subjects.jsx
@@ -4,13 +4,44 @@ import SubjectCard from "../components/SubjectCard";
 
 const modules = [
   {
+    moduleName: "Arabic",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrQflY6FbCbbRtAWx9H6RaiCtOUAYC4keIEQ&usqp=CAU",
+  },
+  {
+    moduleName: "English",
+    img: "https://images.pexels.com/photos/221166/pexels-photo-221166.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    moduleName: "French",
+    img: "https://images.pexels.com/photos/5781917/pexels-photo-5781917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
     moduleName: "Math",
     img: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800",
   },
   {
-    moduleName: "Arabic",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrQflY6FbCbbRtAWx9H6RaiCtOUAYC4keIEQ&usqp=CAU",
+    moduleName: "science",
+    img: "https://images.pexels.com/photos/2280571/pexels-photo-2280571.jpeg?auto=compress&cs=tinysrgb&w=800",
   },
+  {
+    moduleName: "Phisics",
+    img: "https://images.pexels.com/photos/68173/flash-tesla-coil-experiment-faradayscher-cage-68173.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    moduleName: "Islamic",
+    img: "https://images.pexels.com/photos/4346403/pexels-photo-4346403.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    moduleName: "History and geo",
+    img: "https://images.pexels.com/photos/2952871/pexels-photo-2952871.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    moduleName: "civil",
+    img: "https://images.pexels.com/photos/5191376/pexels-photo-5191376.jpeg?auto=compress&cs=tinysrgb&w=800 ",
+  },
+  
+  
+
 ];
 
 const Subjects = () => {
@@ -28,26 +59,11 @@ const Subjects = () => {
                 />
               );
             })}
-            {/* <SubjectCard
-              imageUrl="https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800"
-              subName="Mathemathics"
-            />
-
-            <SubjectCard
-              imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrQflY6FbCbbRtAWx9H6RaiCtOUAYC4keIEQ&usqp=CAU"
-              subName="Arabic"
-            />
-
+            {/* 
             
 
-            <SubjectCard
-              imageUrl="https://images.pexels.com/photos/221166/pexels-photo-221166.jpeg?auto=compress&cs=tinysrgb&w=800"
-              subName="English"
-            />
-            <SubjectCard
-              imageUrl="https://images.pexels.com/photos/2280571/pexels-photo-2280571.jpeg?auto=compress&cs=tinysrgb&w=800"
-              subName="Science"
-            />
+           
+          
 
             <SubjectCard
               imageUrl="https://images.pexels.com/photos/68173/flash-tesla-coil-experiment-faradayscher-cage-68173.jpeg?auto=compress&cs=tinysrgb&w=800"
