aab48a428a4f99f1a1b66ed8f6cb2a276f5f9cc5
Djebril <djebrilacademy@gmail.com>
2023-09-14 03:00:55 +0100

    the click is functional

--- src/pages/dashboard/components/TrimestreBox.jsx
+++ src/pages/dashboard/components/TrimestreBox.jsx
@@ -1,9 +1,29 @@
+export  const TrimestrePopUp = (props)=>{
+
+    return(
+        <section className="w-full" style={{display: props.isPopUpOppened ? "flex": "none"}}>
+            <p>welcome on your first course</p>
+        </section>
+    )
+}
 const TrimestreBox = (props)=>{
+
 return(
+    <>
     <div className="flex items-center justify-between border-2 border-gray rounded-3xl w-full px-16 py-2 shadow-lg ">
         <h1 className="text-3xl capitalize">{props.number} semester</h1>
-        <img className=" cursor-pointer w-8" src="https://cdn-icons-png.flaticon.com/128/2985/2985161.png" alt="" />
+        <img onClick={()=>{
+            if(props.isPopUpOppened){
+                props.switchPopUpState(false)
+                
+            }else{
+                props.switchPopUpState(true)
+            }
+            
+        }} className=" cursor-pointer w-8" src="https://cdn-icons-png.flaticon.com/128/2985/2985161.png" alt="" />
     </div>
+                <TrimestrePopUp isPopUpOppened={props.isPopUpOppened} switchPopUpState={props.switchPopUpState}/>
+    </>
 )
 }
 export default TrimestreBox;
--- src/pages/dashboard/pages/courses-pages/CourseMath.jsx
+++ src/pages/dashboard/pages/courses-pages/CourseMath.jsx
@@ -2,12 +2,14 @@ import TrimestreBox from "../../components/TrimestreBox";
 import { useState } from "react";
 
 const CourseMath = () => {
+    const [isPopUpOppened , switchPopUpState] = useState(false)
+
   return (
     <section className="w-full flex flex-col items-center justify-center gap-4">
 
-      <TrimestreBox number="first"/>
-      <TrimestreBox number="second"/>
-      <TrimestreBox number="third"/>
+      <TrimestreBox number="first"  isPopUpOppened={isPopUpOppened} switchPopUpState={switchPopUpState}/>
+      <TrimestreBox number="second" isPopUpOppened={isPopUpOppened} switchPopUpState={switchPopUpState}/>
+      <TrimestreBox number="third"  isPopUpOppened={isPopUpOppened} switchPopUpState={switchPopUpState}/>
 
     </section>
   );
