cf1aa9ee6952a3176ccc8f1c99d20e2586ea7452
Djebril <djebrilacademy@gmail.com>
2024-02-05 23:40:41 +0100

    done adding the option to the side bar

--- src/pages/dashboard/components/DashboardSideBar.jsx
+++ src/pages/dashboard/components/DashboardSideBar.jsx
@@ -4,31 +4,37 @@ import { useState } from "react";
 
 const DashboardSideBar = () => {
   let isUserLogged = getIsUserLogged();
+  const [BarClosed, setBarState] = useState(false);
   const navigate = useNavigate();
-   const sideBare = ()=>{
-    const [BarState, setBarState] = useState(false)
-    if (BarState) {
-      
+  const sideBare = () => {
+    console.log("im in ");
+    if (BarClosed) {
+      setBarState(false);
+    } else {
+      setBarState(true);
     }
-   }
+  };
 
   return (
     <>
       <section
         id="dashboard-side-bar"
         className="shadow-2xl rounded-2xl py-5  w-80 border-2 ml-4 mt-6 h-full hidden"
+        style={{ display: BarClosed ? "none" : "block" }}
       >
-        <Link Link to="/dashboard/home">
-          <div className="flex justify-end items-center flex-end mx-4">
+        <div
+          style={{ backgroundColor: "#61faaf" }}
+          className="flex justify-end items-center flex-end mx-4 rounded-full "
+        >
           <img
-              src="https://cdn-icons-png.flaticon.com/128/2985/2985161.png"
-              alt=""
-              className="w-12"
-              onClick={sideBare}
-            />
-          </div>
+            src="https://cdn-icons-png.flaticon.com/128/2985/2985161.png"
+            alt=""
+            className="w-12"
+            onClick={sideBare}
+          />
+        </div>
+        <Link Link to="/dashboard/home">
           <div className="flex items-center justify-start border-b-2">
-           
             <img
               className="w-8 m-4 cursor-pointer"
               src="https://cdn-icons-png.flaticon.com/128/3917/3917711.png"
@@ -36,7 +42,6 @@ const DashboardSideBar = () => {
             />
 
             <h1 className="text-3xl my-4 cursor-pointer">Dashboard</h1>
-          
           </div>
         </Link>
 
@@ -113,29 +118,41 @@ const DashboardSideBar = () => {
         </div>
       </section>
 
-      <section className="">
-        <div className="flex justify-center items-center">
-          <img
-            className="w-8"
-            src="https://cdn-icons-png.flaticon.com/128/271/271228.png"
-            alt=""
-          />
-        </div>
+      <section className="" style={{ display: BarClosed ? "block" : "none" }}>
+        <div className="flex justify-center items-center gap-4 flex-col m-4 w-24 border-2 rounded-2xl py-4">
+          <div
+            style={{ backgroundColor: "#61faaf" }}
+            className="flex items-center justify-end   p-4 rounded-full cursor-pointer"
+          >
+            <img
+              className="w-8 "
+              src="https://cdn-icons-png.flaticon.com/128/271/271228.png"
+              alt=""
+              onClick={sideBare}
+            />
+          </div>
 
-        <div className="flex justify-center items-start flex-col m-4 w-24 border-2">
-          <img
-            className="w-20 m-2"
-            src="https://cdn-icons-png.flaticon.com/128/3917/3917711.png"
-            alt=""
-          />
-          <img
-            className="w-12 m-4"
-            src="https://cdn-icons-png.flaticon.com/128/4185/4185218.png"
-          />
+          <Link Link to="/dashboard/home">
+            <img
+              className="w-10 m-2"
+              src="https://cdn-icons-png.flaticon.com/128/3917/3917711.png"
+              alt=""
+            />
+          </Link>
+
+          <Link to="/dashboard/learning-welcome">
+            <img
+              className="w-10 m-4"
+              src="https://cdn-icons-png.flaticon.com/128/4185/4185218.png"
+            />
+          </Link>
+          <Link to="/dashboard/learning-progress">
           <img
             className="w-12 m-4"
             src="https://cdn-icons-png.flaticon.com/128/1535/1535012.png"
           />
+          </Link>
+         
 
           <img
             className="w-12 m-4"
@@ -151,6 +168,19 @@ const DashboardSideBar = () => {
             className="w-12 m-4"
             src="https://cdn-icons-png.flaticon.com/128/5879/5879073.png"
           />
+          <div className="rounded-full p-4 bg-red-500">
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/10477/10477736.png"
+              alt=""
+              className="w-8"
+              onClick={() => {
+                localStorage.setItem("isUserLoggedIn", "false");
+                isUserLogged = getIsUserLogged();
+                console.log(isUserLogged);
+                navigate("/");
+              }}
+            />
+          </div>
         </div>
       </section>
     </>
