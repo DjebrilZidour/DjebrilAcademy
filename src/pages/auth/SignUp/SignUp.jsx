564fba1b0b1cec9745bb1339b93de6bd72cd815d
Djebril <djebrilacademy@gmail.com>
2024-02-24 08:09:31 +0100

    relating singup front with back

--- src/pages/auth/SignUp/SignUp.jsx
+++ src/pages/auth/SignUp/SignUp.jsx
@@ -3,26 +3,53 @@ import Input from "../../../components/Atomic/Input";
 import Btn from "../../../components/Atomic/Btn";
 import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { LOCAL_STORAGE_KEYS } from "../../../utils/utils";
 
 const SignUp = () => {
   const [typedName, setTypedName] = useState("");
   const [typedEmail, setTypedEmail] = useState("");
   const [typedPassword, setTypedPassword] = useState("");
 
+  const createAccount = () => {
+    fetch("http://localhost:3000/register", {
+      method: "POST",
+      body: JSON.stringify({ name:typedName ,email: typedEmail, password: typedPassword }),
+      headers:{
+        Accept:"application/json", "content-Type": "application/json",
+      },
+    })
+    .then((res)=>{
+      if (res.status === 200 ) {
+        console.log("goods");
+        alert("account created successfully")
+        console.log("done sign up ");
+      }else{
+        res.json().then((data)=>{
+          console.log(data);
+          setErrMessage(data.message)
+        })
+      }
+    })
+    .catch((err)=>console.log(err));
+    
+  };
+
   return (
     <div>
       <Navbar />
       <main className="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
         <section>
-          <h3 className="font-bold text-2xl text-center">Welcome to Startup</h3>
+          <h3 className="font-bold text-2xl text-center">
+            Welcome to your online school
+          </h3>
           <p className="text-gray-600 pt-2 text-center">
             Sign up to your account.
           </p>
         </section>
 
         <section className="mt-10">
-          <form className="flex flex-col" method="POST" action="#">
+          <form className="flex flex-col">
             <Input
               label="Full name"
               type="text"
@@ -43,16 +70,7 @@ const SignUp = () => {
             />
             <Btn
               value="sing up"
-              onClick={() => {
-                console.log(
-                  "NAME:",
-                  typedName,
-                  "Email",
-                  typedEmail,
-                  "PASSWORD:",
-                  typedPassword
-                );
-              }}
+              onClick={createAccount}
             />
           </form>
         </section>
@@ -62,7 +80,7 @@ const SignUp = () => {
         <p>
           Already have an account?{" "}
           <Link to="/login">
-            <a href="#" className="font-bold hover:underline">
+            <a className="font-bold hover:underline">
               Sign in .
             </a>
           </Link>
