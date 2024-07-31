df529ce1811380bd985b1e9ec28aff1b468978a2
Djebril <djebrilacademy@gmail.com>
2024-07-31 14:04:21 +0100

    done course exposition structure it still some styling

--- src/pages/dashboard/pages/CoursePreview.jsx
+++ src/pages/dashboard/pages/CoursePreview.jsx
@@ -8,11 +8,13 @@ const CoursePreview = () =>{
     console.log(state.semesterNumber);
     const courseData = fetchSemesterDetail(state.moduleName.toLowerCase() ,state.semesterNumber-1)
     console.log(courseData);
+    const index =state.idx+1
     return (
-        <div className="  w-full  my-4 border-2 border-gray rounded-xl pb-4 ">
+        <div className="  w-full  my-4 border-2 border-gray rounded-xl pb-4 bg-red-100">
       <div className="font-bold  p-4 w-full rounded-t-xl background ">
         <h1 className="text-xl capitalize">{state.moduleName}</h1>
-        <h1 className="text-xl capitalize">{state.idx}</h1>
+        <h1 className="text-xl capitalize">{index}</h1>
+        <h1 className="text-5xl">{courseData[state.idx].courseTitle}</h1>
       </div>
 
      
