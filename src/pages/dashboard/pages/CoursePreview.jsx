0f44f728046722156cb9bf5764af0433b227c625
Djebril <djebrilacademy@gmail.com>
2024-08-02 00:58:23 +0100

    make the preview course page better withote the pdf previewer

--- src/pages/dashboard/pages/CoursePreview.jsx
+++ src/pages/dashboard/pages/CoursePreview.jsx
@@ -17,8 +17,7 @@ const CoursePreview = () => {
   const course = courseData[state.idx];
   console.log("hawlik lanne", state.grade);
   return (
-    <div className="  w-full  my-4 border-none rounded-xl pb-4 h-screen">
-    
+    <>
       <div className=" w-full rounded-xl flex justify-between items-center h-32  drop-shadow-xl">
         <div
           style={{ backgroundImage: `url(${state.imgUrl})` }}
@@ -31,19 +30,34 @@ const CoursePreview = () => {
         <div className="text-2xl font-bold bg-green-100 px-4 w-1/5 h-full px-4 py-8 border-0 rounded-tr-xl flex justify-between items-center">
           {" "}
           <h1> Triméstre {state.semesterNumber} </h1>
-          <h1 className="text-3xl uppercase background border-black border-2 px-2 py-5 cursor-pointer rounded-full ">{state.grade}AS</h1>
+          <h1 className="text-3xl uppercase background border-black border-2 px-2 py-5 cursor-pointer rounded-full ">
+            {state.grade}AS
+          </h1>
         </div>
       </div>
-      <div className="flex justify-center items-center bg-red-100 rounded-b-3xl shadow-xl">
+      <div className="flex justify-center items-center bg-red-100 rounded-b-3xl shadow-xl w-full">
         <h1 className="text-5xl py-4 capitalize">{course.courseTitle}</h1>
-        <h1 className="text-6xl text-red-500">marhba {state.grade} AS</h1>
       </div>
-    
-    <div className="flex justify-center">
-       
-    </div>
-    </div>
-    
+      <div className="  w-full   border-none rounded-xl my-8 flex justify-center items-center flex-col">
+        <div className="flex justify-between items-center w-full">
+          <div className="my-12 mx-8 flex flex-col justify-center items-start gap-4 w-1/2">
+            <h1 className="text-6xl text-red-500 capitalize">
+              Description de Cours
+            </h1>
+            <p className="w-5/6 text-lg">{course.courseDetail}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center w-1/2 gap-2">
+            <img
+              className="w-1/3"
+              src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
+              alt=""
+            />
+            <h1 className="text-2xl text-red-700 font-bold">!!PDF PREVIEW</h1>
+          </div>
+        </div>
+        <Btn value="GET PDF FILE" />
+      </div>
+    </>
   );
 };
 
