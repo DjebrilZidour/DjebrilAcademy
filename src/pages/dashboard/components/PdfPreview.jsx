5f1235b2820a0efaece389477a04b1acb8efc93c
Djebril <djebrilacademy@gmail.com>
2024-08-05 02:01:50 +0100

    delete some not interesting things => everything GOOD

--- src/pages/dashboard/components/PdfPreview.jsx
+++ src/pages/dashboard/components/PdfPreview.jsx
@@ -9,11 +9,10 @@ const PdfPreview = ({ pdfUrl }) => {
 
   return (
     <div className="my-8" style={{ display: 'flex', width: '80%', margin: '0 auto', border:'none'}}>
-      {/* Sidebar (conditionally rendered) */}
+
       {showSidebar && (
-        <div style={{ width: '250px', borderRight: '1px solid #ddd', padding: '10px' }}>
-          <h3>Sidebar</h3>
-          <p>Additional controls or information here.</p>
+        <div style={{ width: '250px', borderRight: 'none', padding: '10px' }}>
+          
         </div>
       )}
       
@@ -29,17 +28,7 @@ const PdfPreview = ({ pdfUrl }) => {
         ></iframe>
       </div>
    
-      <button
-        onClick={toggleSidebar}
-        style={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          zIndex: 1000
-        }}
-      >
-        {showSidebar ? 'Hide Sidebar' : 'Show Sidebar'}
-      </button>
+      
     </div>
   );
 };
