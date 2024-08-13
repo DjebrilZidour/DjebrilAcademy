2e1765942815b2b66c14dbf3cb3fa53d372c68bf
Djebril <djebrilacademy@gmail.com>
2024-08-14 00:40:12 +0100

    add new course

--- src/api/index.js
+++ src/api/index.js
@@ -28,6 +28,12 @@ const dataBase = [
           courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
           pdfUrl: "",
         },
+        {
+          courseTitle: "cour jdid",
+          courseDetail: " hani creto dok he breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        },
       ],
       [
         {
@@ -68,7 +74,7 @@ const dataBase = [
         {
           courseTitle: "course phisics title sem 1",
           courseDetail: "course phisics detail sem 1",
-          
+
         },
       ],
       [
