2a7c767d782063ae5517568cb939ab884a4d0977
Djebril <djebrilacademy@gmail.com>
2024-08-13 13:25:21 +0100

    editing text position

--- src/pages/dashboard/pages/Grade.jsx
+++ src/pages/dashboard/pages/Grade.jsx
@@ -14,7 +14,7 @@ const Grade = () => {
 
   return (
     <section className="flex flex-col justify-center items-center gap-8 py-12">
-      <h1 className="text-4xl mt-4 ">S.V.P Selectionne Votre Niveaux scolaire</h1>
+      <h1 className="text-4xl mt-4 text-center">S.V.P Selectionne Votre Niveaux scolaire</h1>
       <button className="text-3xl uppercase background p-3 cursor-pointer rounded-tl-md rounded-tr-md rounded-br-3xl rounded-bl-md hover:scale-105 cursor-pointer duration-200  " onClick={()=>{
         
         navigate("/dashboard/learning-welcome", {
