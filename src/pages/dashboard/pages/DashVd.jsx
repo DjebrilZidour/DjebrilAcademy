bf60807ade855e9ab32e9599adfab402e3c24ec9
Djebril <djebrilacademy@gmail.com>
2024-08-22 21:22:39 +0100

    fixing the style of the position of the videos

--- src/pages/dashboard/pages/DashVd.jsx
+++ src/pages/dashboard/pages/DashVd.jsx
@@ -3,9 +3,11 @@ const DashVd = () =>{
         <>
         <section className=" flex flex-col justify-center items-center gap-4">
             <div className="flex justify-center gap-4 p-4">
-               <div className="border-2 p-4">1</div>
-               <div className="border-2 p-4">2</div>
-               <div className="border-2 p-4">3</div>
+               <div className="border-2 p-4 w-1/3">
+               <iframe className="w-full" src="https://www.youtube.com/embed/P0uaLRO6V1U" title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+               </div>
+               <div className="border-2 p-4 w-1/3"><iframe  className="w-full" src="https://www.youtube.com/embed/P0uaLRO6V1U" title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
+               <div className="border-2 p-4 w-1/3"><iframe  className="w-full" src="https://www.youtube.com/embed/P0uaLRO6V1U" title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
             </div>
             <div className="flex justify-center gap-4 p-4">
                <div className="border-2 p-4">4</div>
