d7746671c792528bdb4bf18d443ac0ad5aa394a6
Djebril <djebrilacademy@gmail.com>
2024-08-23 11:24:59 +0100

    fixi,g the way of exposing the videos

--- src/pages/dashboard/pages/DashVd.jsx
+++ src/pages/dashboard/pages/DashVd.jsx
@@ -1,27 +1,118 @@
-const DashVd = () =>{
-    return(
-        <>
-        <section className=" flex flex-col justify-center items-center gap-4">
-            <div className="flex justify-center gap-4 p-4">
-               <div className="border-2 p-4 w-1/3">
-               <iframe className="w-full" src="https://www.youtube.com/embed/P0uaLRO6V1U" title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-               </div>
-               <div className="border-2 p-4 w-1/3"><iframe  className="w-full" src="https://www.youtube.com/embed/P0uaLRO6V1U" title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
-               <div className="border-2 p-4 w-1/3"><iframe  className="w-full" src="https://www.youtube.com/embed/P0uaLRO6V1U" title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
-            </div>
-            <div className="flex justify-center gap-4 p-4">
-               <div className="border-2 p-4">4</div>
-               <div className="border-2 p-4">5</div>
-               <div className="border-2 p-4">6</div>
-            </div>
-
-            <div className="flex justify-center gap-4 p-4">
-               <div className="border-2 p-4">7</div>
-               <div className="border-2 p-4">8</div>
-               <div className="border-2 p-4">9</div>
-            </div>
-        </section>
-        </>
-    )
-}
-export default DashVd
+const DashVd = () => {
+  return (
+    <>
+      <section className=" flex flex-col justify-center items-center gap-4">
+        <h1 className="text-3xl">Tandim el wakt </h1>
+        <div className="flex justify-center gap-4 p-2">
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+        <h1 className="text-3xl">Indibat </h1>
+        <div className="flex justify-center gap-4 p-2">
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+        <h1 className="text-3xl">advice </h1>
+        <div className="flex justify-center gap-4 p-2">
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="w-1/3">
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/P0uaLRO6V1U"
+              title="سورة البقرة كاملة للشيخ د. ياسر الدوسري~طاردة الشياطين | Surat Al-Baqarah | Yasser Al-Dosari"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      
+      </section>
+    </>
+  );
+};
+export default DashVd;
