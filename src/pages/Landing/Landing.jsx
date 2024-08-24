bb01cd358d6c9a59efc354589bcf629e57cc652f
Djebril <djebrilacademy@gmail.com>
2024-08-24 01:09:40 +0100

    added a good part for the ceo

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -11,16 +11,17 @@ const Landing = () => {
   return (
     <>
       <Navbar />
- 
-      <section className="flex items-center justify-center font-sans w-full pb-20 mt-8">
+
+      <section className="flex items-center justify-center font-sans w-full pb-4 mt-8">
         <div className=" w-full flex items-center justify-center gap-16 md:flex-row flex-col">
           <div>
             <div className="mb-8 text-center lg:mx-0  md:text-left ">
               <h2 className="capitalize md:text-4xl text-2xl md:w-92 text-center md:text-start ">
                 welcome to <br />{" "}
-                <span className="text-green-400 text-4xl">Djebril Academy</span> <br />
+                <span className="text-green-400 text-4xl">Djebril Academy</span>{" "}
+                <br />
                 The first platform to fix your mathemathics and phisics{" "}
-                <span className="text-red-400 text-4xl">problems</span>
+                <span className="text-red-400 text-3xl">problems</span>
               </h2>
 
               <p className="text-center md:text-start visible mx-0 mt-3 mb-0 text-sm leading-relaxed text-left text-slate-400">
@@ -77,8 +78,8 @@ const Landing = () => {
       </section> */}
 
       <section className="my-10 py-2">
-        <h1 className="text-3xl font-semibold text-center ">
-          what is our mission
+        <h1 className="text-5xl font-semibold text-center  capitalize p-4">
+          our mission
         </h1>
         <div className="flex justify-between items-center mx-12 lg:mx-28 my-2 flex-col md:flex-row gap-4 md:gap-0">
           <div className="flex flex-col items-center justify-center">
@@ -87,7 +88,7 @@ const Landing = () => {
               src="https://cdn-icons-png.flaticon.com/512/9831/9831318.png"
               alt=""
             />
-            <h1 className="text-xl w-40 text-center">
+            <h1 className="text-xl w-40 text-center capitalize">
               understand all your lessons in one platform
             </h1>
           </div>
@@ -102,8 +103,8 @@ const Landing = () => {
               src="https://cdn-icons-png.flaticon.com/512/10266/10266372.png"
               alt=""
             />
-            <h1 className="text-xl w-40 text-center">
-              practisce what did you learned
+            <h1 className="text-xl w-40 text-center capitalize">
+              practisce all what did you learned
             </h1>
           </div>
           <img
@@ -111,13 +112,13 @@ const Landing = () => {
             src="https://cdn-icons-png.flaticon.com/128/1294/1294758.png"
             alt=""
           />
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center ">
             <img
               className="w-20"
               src="https://cdn-icons-png.flaticon.com/512/5243/5243421.png"
               alt=""
             />
-            <h1 className="text-xl w-40 text-center">
+            <h1 className="text-xl w-40 text-center capitalize">
               get the highest mark in your exams
             </h1>
           </div>
@@ -133,22 +134,40 @@ const Landing = () => {
       </section> */}
 
       <section className="flex flex-col justify-center items-center gap-4 my-12 mx-2">
-        <h1 className="text-4xl underline">Our ceo</h1>
+      <h1 className="text-5xl font-semibold text-center  capitalize">
+          our ceo
+        </h1>
         <div className="flex justify-center items-center w-full md:flex-row flex-col gap-8 my-8 md:px-32  ">
           <div className="flex flex-col  items-center md:items-start md:w-1/2 md:p-4 gap-8">
-            <h1 className="text-2xl md:text-5xl">Zidour Djebril</h1>
+            <h1 className="text-3xl md:text-5xl font-semi-bold">Zidour Djebril</h1>
             <p className="text-lg md:text-xl text-center md:text-start ">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
-              praesentium dicta soluta autem dignissimos quae dolore molestiae
-              deleniti error obcaecati provident quam, cum porro recusandae et
-              tempora laudantium doloremque ipsum!
+            "To me, self-improvement is a journey that demands both intelligence and hard work, but above all, discipline. Without discipline, even the best intentions fall short."
             </p>
-             <p>social media </p>
+            <p className="text-lg md:text-xl text-center md:text-start ">
+            "بالنسبة لي، تحسين الذات هو رحلة تتطلب الذكاء والعمل الجاد، ولكن الأهم من ذلك هو الانضباط. بدون الانضباط، حتى أفضل النوايا لا تحقق النجاح."
+            </p>
+            <p>social media </p>
             <div className="flex gap-4">
-              <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png" alt="" />
-              <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png" alt="" />
-              <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png" alt="" />
-              <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png" alt="" />
+              <img
+                className="w-12"
+                src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png"
+                alt=""
+              />
+              <img
+                className="w-12"
+                src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png"
+                alt=""
+              />
+              <img
+                className="w-12"
+                src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png"
+                alt=""
+              />
+              <img
+                className="w-12"
+                src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png"
+                alt=""
+              />
             </div>
           </div>
           <div className="md:w-1/2 w-4/5 flex justify-center items-center">
@@ -178,7 +197,7 @@ const Landing = () => {
           <Btn value="See more" />
         </Link>
       </section>
-      
+
       {/* <section>
         <h1>what people sad about us </h1> //! check the drawed paper // check american institute website 
         <div></div>
