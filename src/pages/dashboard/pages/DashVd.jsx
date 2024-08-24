e3a0832ba1ab76ddd615d70b4a86e96676055bbf
Djebril <djebrilacademy@gmail.com>
2024-08-24 11:56:52 +0100

    re add the videos deploying bug fixed

--- src/pages/dashboard/pages/DashVd.jsx
+++ src/pages/dashboard/pages/DashVd.jsx
@@ -3,7 +3,7 @@ const DashVd = () => {
     <>
       <section className=" flex flex-col justify-center items-center gap-4">
         <h1 className="text-3xl">Tandim El wakt </h1>
-        {/* <div className="flex md:flex-row flex-col justify-center gap-4 p-2">
+        <div className="flex md:flex-row flex-col justify-center gap-4 p-2">
           <div className="md:w-1/3 w-full">
             <iframe
               className="w-full rounded-3xl"
@@ -28,33 +28,89 @@ const DashVd = () => {
           </div>
 
           <div className="md:w-1/3 w-full">
-          <iframe className="w-full rounded-3xl" src="https://www.youtube.com/embed/DgZ9Xfc7GOU" title="BrainerX Podcast Episode 02 : L&#39;éducation en Algerie التعليم في الجزائر" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/DgZ9Xfc7GOU"
+              title="BrainerX Podcast Episode 02 : L&#39;éducation en Algerie التعليم في الجزائر"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
         <h1 className="text-3xl">Indibat </h1>
         <div className="flex md:flex-row flex-col justify-center gap-4 p-2">
           <div className="md:w-1/3 w-full">
-          <iframe className="w-full rounded-3xl" src="https://www.youtube.com/embed/1UEjZHfFAJk" title="أكبر خطأ يرتكبه الإنسان، توقّف عن إرضاء الآخرين | بوكافيين" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/1UEjZHfFAJk"
+              title="أكبر خطأ يرتكبه الإنسان، توقّف عن إرضاء الآخرين | بوكافيين"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
           <div className="md:w-1/3 w-full">
-          <iframe className="w-full rounded-3xl" src="https://www.youtube.com/embed/ZPnIq2ZQgOU" title="تحديد الأولويات: الخطوة الأولى نحو التحكم بحياتك | ياسر الحزيمي" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/ZPnIq2ZQgOU"
+              title="تحديد الأولويات: الخطوة الأولى نحو التحكم بحياتك | ياسر الحزيمي"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
           <div className="md:w-1/3 w-full">
-          <iframe className="w-full rounded-3xl" src="https://www.youtube.com/embed/VpQn48MV1Js" title="كيف تكون مبدع في ادارة الوقت ⏰ | اول شيء لازم تبدأ فيه بحياتك" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/VpQn48MV1Js"
+              title="كيف تكون مبدع في ادارة الوقت ⏰ | اول شيء لازم تبدأ فيه بحياتك"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
         <h1 className="text-3xl">Advice </h1>
         <div className="flex md:flex-row flex-col justify-center gap-4 p-2">
           <div className="md:w-1/3 w-full">
-          <iframe className="w-full rounded-3xl" src="https://www.youtube.com/embed/J-6MlapnHwM" title="الحل الوحيد عشان تلتزم بالجدول في حياتك؟" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/J-6MlapnHwM"
+              title="الحل الوحيد عشان تلتزم بالجدول في حياتك؟"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
           <div className="md:w-1/3 w-full">
-          <iframe className="w-full rounded-3xl" src="https://www.youtube.com/embed/oQ11lpnicu4" title="&quot;كيفية التخطيط الفعّال للباكالوريا: استراتيجيات ونصائح للنجاح&quot;&quot;أفضل طرق التخطيط للباكالوريا:" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/oQ11lpnicu4"
+              title='"كيفية التخطيط الفعّال للباكالوريا: استراتيجيات ونصائح للنجاح""أفضل طرق التخطيط للباكالوريا:'
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
           <div className="md:w-1/3 w-full">
-          <iframe className="w-full rounded-3xl" src="https://www.youtube.com/embed/LvYki1R6BsM" title="خطة و برنامج دراسي لشهر سبتمبر من اجل الحصول على 19 في بكالوريا 2025 | استعد للدخول المدرسي bac 2025" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+              className="w-full rounded-3xl"
+              src="https://www.youtube.com/embed/LvYki1R6BsM"
+              title="خطة و برنامج دراسي لشهر سبتمبر من اجل الحصول على 19 في بكالوريا 2025 | استعد للدخول المدرسي bac 2025"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
-        </div> */}
+        </div>
       </section>
     </>
   );
