385f48a6172acc32f645bf763f91c07ea01d11db
Djebril <djebrilacademy@gmail.com>
2024-08-24 02:00:07 +0100

    fix margins and paddings between sections

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -12,7 +12,7 @@ const Landing = () => {
     <>
       <Navbar />
 
-      <section className="flex items-center justify-center font-sans w-full py-12 mt-12 my-16 ">
+      <section className="flex items-center justify-center font-sans w-full py-12 my-16 ">
         <div className=" w-full flex items-center justify-center gap-16 md:flex-row flex-col md:mx-48">
           <div className="md:w-1/2">
             <div className="mb-8 text-center lg:mx-0  md:text-left ">
@@ -54,30 +54,9 @@ const Landing = () => {
         </div>
       </section>
 
-      {/* <section className="flex justify-between items-center p-4 mx-24 h-92 gap-4">
-        <div className="borders border-2 rounded-3xl flex flex-col justify-start items-start w-1/2 h-96 gap-6 p-4 ">
-          <h1 className="text-2xl underline">Why choose us !!!</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-            veritatis quos recusandae et, voluptas harum nam tempore expedita
-            sed dolorum. Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Fuga iste accusantium dolorem quasi a doloremque nesciunt,
-            repellendus est dicta officiis obcaecati soluta veritatis quod nulla
-            incidunt harum quisquam vitae auto
-          </p>
-          <Btn value="learn more" />
-        </div>
+    
 
-        <div className=" w-1/2 h-96">
-          <img
-            className="w-full  h-full  rounded-3xl border-2  borders"
-            src="https://images.pexels.com/photos/3728084/pexels-photo-3728084.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-          />
-        </div>
-      </section> */}
-
-      <section className="my-10 py-2">
+      <section className="my-10 py-16">
         <h1 className="text-5xl font-semibold text-center  capitalize p-4">
           our mission
         </h1>
