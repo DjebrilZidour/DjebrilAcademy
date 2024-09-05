fbb59e404a29eed9148cb20b700bed8dd7f7a0bf
Djebril <djebrilacademy@gmail.com>
2024-09-05 21:15:46 +0200

    good done reversing the dashboard for arabic

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,3 @@
-// src/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
@@ -18,6 +17,13 @@ const resources = {
       "our_ceo": "Our CEO",
       "our_team": "Our Team",
       "see_more": "See more",
+      "todo": "TO DO",
+      "new_task": "New Task",
+      "add_task": "Add Task",
+      "toggle_task_done": "Toggle Task Done",
+      "delete_task": "Delete Task",
+      "still_for_2025": "Still for 2025",
+      "error_invalid_task": "Please enter a valid task"
     }
   },
   fr: {
@@ -35,13 +41,20 @@ const resources = {
       "our_ceo": "Notre PDG",
       "our_team": "Notre Équipe",
       "see_more": "Voir plus",
+      "todo": "À FAIRE",
+      "new_task": "Nouvelle Tâche",
+      "add_task": "Ajouter Tâche",
+      "toggle_task_done": "Changer Statut Tâche",
+      "delete_task": "Supprimer Tâche",
+      "still_for_2025": "Encore pour 2025",
+      "error_invalid_task": "Veuillez entrer une tâche valide"
     }
   },
   ar: {
     translation: {
       "welcome": "مرحبًا بكم في  ",
       "welcomeName": "جبريل اكاديمي",
-      "des2":"أول منصة لحل",
+      "des2": "أول منصة لحل",
       "problems": " مشكلات",
       "platform_desc": " الرياضيات والفيزياء الخاصة بك",
       "get_course": "احصل على الدورة/التمارين",
@@ -53,6 +66,13 @@ const resources = {
       "our_ceo": "مديرنا التنفيذي",
       "our_team": "فريقنا",
       "see_more": "شاهد المزيد",
+      "todo": "قائمة المهام",
+      "new_task": "مهمة جديدة",
+      "add_task": "أضف مهمة",
+      "toggle_task_done": "تبديل حالة المهمة",
+      "delete_task": "حذف مهمة",
+      "still_for_2025": "لا يزال للعام 2025",
+      "error_invalid_task": "يرجى إدخال مهمة صحيحة"
     }
   }
 };
@@ -61,7 +81,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: "ar", // langue par défaut
+    lng: "ar", // Default language
     interpolation: {
       escapeValue: false,
     },
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,102 +1,81 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import DashboardSideBar from "./components/DashboardSideBar";
 import Navbar from "../../components/Navbar";
 import Btn from "../../components/Atomic/Btn";
 import Input from "../../components/Atomic/Input";
 import ResponsiveSideBar from "./components/ResponsiveSideBar";
 import Countdown from "./components/Countdown";
+import i18n from "../../i18n";
 
 const TodoList = () => {
+  const { t } = useTranslation();
   const [typedTask, setTypedTask] = useState("");
   const [todoList, setTodoList] = useState([]);
   const [isTaskDone, setTaskDone] = useState(false);
-  let i = 0;
 
   const setTaskDoneBtn = () => {
-    if (isTaskDone) {
-      setTaskDone(false);
-    } else {
-      setTaskDone(true);
-    }
+    setTaskDone(!isTaskDone);
   };
 
   const addTodo = () => {
-    console.log(typedTask);
     if (typedTask.length < 4) {
-      console.log("pls enter a valid task");
+      console.log(t('error_invalid_task'));
     } else {
-      todoList.push(typedTask);
+      setTodoList([...todoList, typedTask]);
       setTypedTask("");
-      console.log(todoList);
-      setTodoList(todoList);
     }
   };
 
   return (
-    <>
-      <div className="border-2 rounded-3xl p-6 shadow-3xl px-16 h-96">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-3xl">TO DO</h1>
-
-          <Input
-            label="new task"
-            type="text"
-            value={typedTask}
-            setValue={setTypedTask}
-          />
-
-          <Btn value="newTodo" onClick={addTodo} />
-          <hr className=" border-1 border-black w-full rounded-3xl m-4" />
-
-          <div>
-            {todoList.map((element, i) => {
-              return (
-                <ul className=" flex justify-between items-center gap-8">
-                  <li className={isTaskDone ? "" : ""} key={i}>
-                    {element}
-                  </li>
-                  <div className="flex justify-between items-center gap-4">
-                    <img
-                      key={i}
-                      onClick={setTaskDoneBtn}
-                      className="w-4 h-4 rounded-full cursor-pointer"
-                      src={
-                        isTaskDone
-                          ? "https://cdn-icons-png.flaticon.com/128/7739/7739845.png"
-                          : "https://cdn-icons-png.flaticon.com/128/808/808569.png"
-                      }
-                      alt=""
-                    />
-                    <img
-                      onClick={() => {}}
-                      className="w-4 h-4 rounded-full cursor-pointer"
-                      src="https://cdn-icons-png.flaticon.com/128/594/594864.png"
-                      alt=""
-                    />
-                  </div>
-                  {/* <img src={TodoList?"":""} alt="" /> */}
-                </ul>
-              );
-            })}
-          </div>
+    <div className="border-2 rounded-3xl p-6 shadow-3xl px-16 h-96">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-3xl">{t('todo')}</h1>
+        <Input
+          label={t('new_task')}
+          type="text"
+          value={typedTask}
+          setValue={setTypedTask}
+        />
+        <Btn value={t('add_task')} onClick={addTodo} />
+        <hr className="border-1 border-black w-full rounded-3xl m-4" />
+        <div>
+          {todoList.map((element, i) => (
+            <ul className="flex justify-between items-center gap-8" key={i}>
+              <li className={isTaskDone ? "line-through" : ""}>{element}</li>
+              <div className="flex justify-between items-center gap-4">
+                <img
+                  onClick={setTaskDoneBtn}
+                  className="w-4 h-4 rounded-full cursor-pointer"
+                  src={isTaskDone ? "https://cdn-icons-png.flaticon.com/128/7739/7739845.png" : "https://cdn-icons-png.flaticon.com/128/808/808569.png"}
+                  alt={t('toggle_task_done')}
+                />
+                <img
+                  onClick={() => {}}
+                  className="w-4 h-4 rounded-full cursor-pointer"
+                  src="https://cdn-icons-png.flaticon.com/128/594/594864.png"
+                  alt={t('delete_task')}
+                />
+              </div>
+            </ul>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
-
 const Dashboard = () => {
+  const { t } = useTranslation();
+  const langDirection = i18n.language === 'ar' ? 'row-reverse' : 'row';
   return (
     <>
       <Navbar />
-
-      <section className="flex items-start justify-between">
+      <section style={{flexDirection:langDirection}} className="flex items-start justify-between">
         <div className="hidden lg:flex">
           <DashboardSideBar />
         </div>
-
-        <div className="border-4 border-gray lg:mx-4 mb-4 pb-4 mt-6 rounded-3xl flex flex-col items-center justify-center w-full drop-shadow-lg bg-green-50  p-2">
+        <div className="border-4 border-gray lg:mx-4 mb-4 pb-4 mt-6 rounded-3xl flex flex-col items-center justify-center w-full drop-shadow-lg bg-green-50 p-2">
           <div className="md:hidden">
             <ResponsiveSideBar />
           </div>
@@ -111,19 +90,17 @@ export default Dashboard;
 
 // todo : move to pages
 export const Default = () => {
+  const { t } = useTranslation();
   const targetDate = new Date("2024-12-31T00:00:00");
   return (
-    <>
-      <div className=" flex md:gap-4 flex-col md:flex-row">
-        <div className="flex justify-between items-start gap-4 mt-4 w-full h-full ">
-          <div className=" flex flex-col justify-center items-center gap-2 border-2 rounded-3xl p-6 my-4">
-            <h1 className="text-2xl">Still for 2025</h1>
-
-            <Countdown targetDate={targetDate} />
-          </div>
+    <div className="flex md:gap-4 flex-col md:flex-row">
+      <div className="flex justify-between items-start gap-4 mt-4 w-full h-full">
+        <div className="flex flex-col justify-center items-center gap-2 border-2 rounded-3xl p-6 my-4">
+          <h1 className="text-2xl">{t('still_for_2025')}</h1>
+          <Countdown targetDate={targetDate} />
         </div>
-        <TodoList />
       </div>
-    </>
+      <TodoList />
+    </div>
   );
 };
