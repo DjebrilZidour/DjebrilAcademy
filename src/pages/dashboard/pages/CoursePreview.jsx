d3ab4a046b9647e7a37cd400b689a1287fc00128
Djebril <djebrilacademy@gmail.com>
2024-09-11 19:58:54 +0200

    start tring to fix the style

--- src/pages/dashboard/pages/CoursePreview.jsx
+++ src/pages/dashboard/pages/CoursePreview.jsx
@@ -12,19 +12,34 @@ const CoursePreview = () => {
 
   return (
     <>
+   <div className="rounded-xl w-full rounded-xl flex md:flex-row flex-col justify-between items-center h-64  drop-shadow-2xl">
       <div className="rounded-xl w-full flex justify-between items-center h-64">
         <div
+          style={{ backgroundImage: `url(${state.imgUrl})` }}
+          className="text-5xl text-white font-bold bg-cover  py-4 w-4/5 h-full px-4 py-8  rounded-tl-xl flex justify-start items-center"
           style={{ backgroundImage: `url(${imgUrl})` }}
           className="text-5xl text-white font-bold bg-cover py-4 w-4/5 h-full px-4 rounded-tl-xl"
         >
+          {" "}
+          <h1> {state.moduleName} </h1>
           <h1>{moduleName}</h1>
         </div>
 
+        <div className="text-2xl font-bold bg-green-100 px-4 w-4/5 md:w-2/6 md:h-full px-4 py-2 md:py-8  rounded-b-xl flex justify-center  gap-4 items-center flex-col">
+          {" "}
+          <h1 className="text-md"> Triméstre {state.semesterNumber} </h1>
+          <h1 className="text-3xl uppercase background border-black border-2 px-2 py-2 md:py-5 cursor-pointer rounded-full ">
+            {state.grade}AS
+          </h1>
         <div className="text-2xl font-bold bg-green-100 px-4 w-4/5 md:w-2/6 h-full flex justify-center items-center gap-4">
           <h1>Triméstre {semesterNumber}</h1>
           <h1 className="text-3xl uppercase border-2 px-2 py-2">{grade} AS</h1>
         </div>
       </div>
+      <div className="flex justify-center w-4/5 items-center bg-red-100 rounded-b-3xl shadow-xl md:w-full">
+        <h1 className="md:text-5xl text-4xl text-center py-4 capitalize">
+          {course.courseTitle}
+        </h1>
       <div className="flex justify-center items-center bg-red-100 shadow-xl">
         <h1 className="text-5xl py-4 capitalize">{course.courseTitle}</h1>
       </div>
