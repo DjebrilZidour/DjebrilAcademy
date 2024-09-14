967ebdcd7e4abdcc8624a49e20b47683ab92eca5
Djebril <djebrilacademy@gmail.com>
2024-09-14 23:45:49 +0200

    re adding the other sections of the landing.jsx

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -123,6 +123,133 @@ const Landing = ({ language }) => {
         </div>
       </section>
       {/* Other sections */}
+      <section className="my-10 py-16">
+        <h1 className="text-5xl font-semibold text-center capitalize p-4">
+          {t("our_mission")}
+        </h1>
+        <div className="flex justify-between items-center mx-12 lg:mx-28 my-2 flex-col md:flex-row gap-4 md:gap-0">
+          <div className="flex flex-col items-center justify-center">
+            <img
+              className="w-20"
+              src="https://cdn-icons-png.flaticon.com/512/9831/9831318.png"
+              alt=""
+            />
+            <h1 className="text-xl w-40 text-center capitalize">
+              {t("understand_lessons")}
+            </h1>
+          </div>
+          <img
+            className="w-12 md:w-20"
+            src="https://cdn-icons-png.flaticon.com/128/1294/1294758.png"
+            alt=""
+          />
+          <div className="flex flex-col items-center justify-center">
+            <img
+              className="w-20"
+              src="https://cdn-icons-png.flaticon.com/512/10266/10266372.png"
+              alt=""
+            />
+            <h1 className="text-xl w-40 text-center capitalize">
+              {t("practice_learned")}
+            </h1>
+          </div>
+          <img
+            className="w-12 md:w-20"
+            src="https://cdn-icons-png.flaticon.com/128/1294/1294758.png"
+            alt=""
+          />
+          <div className="flex flex-col items-center justify-center">
+            <img
+              className="w-20"
+              src="https://cdn-icons-png.flaticon.com/512/5243/5243421.png"
+              alt=""
+            />
+            <h1 className="text-xl w-40 text-center capitalize">
+              {t("highest_mark")}
+            </h1>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col justify-center items-center gap-4 my-12 mx-2">
+        <h1 className="text-5xl font-semibold text-center  capitalize">
+          {t("our_ceo")}
+        </h1>
+        <div className="flex justify-center items-center w-full md:flex-row flex-col gap-8 my-8 md:px-32  ">
+          <div className="flex flex-col  items-center md:items-start md:w-1/2 md:p-4 gap-8">
+            <h1 className="text-3xl md:text-5xl font-semi-bold">
+              Zidour Djebril
+            </h1>
+            <p className="text-lg md:text-xl text-center md:text-start ">
+              "To me, self-improvement is a journey that demands both
+              intelligence and hard work, but above all, discipline. Without
+              discipline, even the best intentions fall short."
+            </p>
+            <p className="text-lg md:text-xl text-center md:text-start ">
+              "بالنسبة لي، تحسين الذات هو رحلة تتطلب الذكاء والعمل الجاد، ولكن
+              الأهم من ذلك هو الانضباط. بدون الانضباط، حتى أفضل النوايا لا تحقق
+              النجاح."
+            </p>
+
+            <div className="flex gap-8">
+              <a href="https://github.com/DjebrilZidour" target="new">
+                <img
+                  className="w-12"
+                  src="https://cdn-icons-png.flaticon.com/128/25/25231.png"
+                  alt=""
+                />
+              </a>
+              <a href="mailto:djebril.zidour.pro@gmail.com" target="new">
+                <img
+                  className="w-12"
+                  src="https://cdn-icons-png.flaticon.com/128/542/542689.png"
+                  alt=""
+                />
+              </a>
+              <a href="https://djebril.pages.dev/" target="new">
+                <img
+                  className="w-12"
+                  src="https://cdn-icons-png.flaticon.com/128/5891/5891729.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+          <div className="md:w-1/2 w-4/5 flex justify-center items-center">
+            <img
+              class
+              src="https://i.ibb.co/9WyBmLY/Design-sans-titre-1.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <h1 className="text-5xl font-semibold text-center  capitalize p-4">
+          {t("our_team")}
+        </h1>
+        <div className="flex justify-center items-center md:gap-4 flex-col md:flex-row mx-4">
+          <TeamCard
+            name="Prof1 "
+            description="description prof 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi labore doloribus deleniti repudiandae ."
+            img="https://images.pexels.com/photos/8720598/pexels-photo-8720598.jpeg?auto=compress&cs=tinysrgb&w=800"
+          />
+          <TeamCard
+            name="Prof2"
+            description="description prof 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi labore doloribus deleniti repudiandae ."
+            img="https://images.pexels.com/photos/8720598/pexels-photo-8720598.jpeg?auto=compress&cs=tinysrgb&w=800"
+          />
+          <TeamCard
+            name="Prof3"
+            description="description prof 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi labore doloribus deleniti repudiandae ."
+            img="https://images.pexels.com/photos/8720598/pexels-photo-8720598.jpeg?auto=compress&cs=tinysrgb&w=800"
+          />
+        </div>
+        <Link to="/team">
+          <Btn value="See more" />
+        </Link>
+      </section>
+
+      <Footer />
+      {/* Other sections */}
     </>
   );
 };
