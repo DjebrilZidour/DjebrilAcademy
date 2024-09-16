e372b05d0d6ebb66f133c538fa1721aa5ba96eb8
Djebril <djebrilacademy@gmail.com>
2024-09-16 16:34:22 +0200

    update the path for the responsive side bar

--- src/pages/dashboard/components/ResponsiveSideBar.jsx
+++ src/pages/dashboard/components/ResponsiveSideBar.jsx
@@ -1,38 +1,36 @@
 import { Link } from "react-router-dom";
-const ResponsiveSideBar = ()=>{
-    return(
-        <section className="w-full">
+const ResponsiveSideBar = () => {
+  return (
+    <section className="w-full">
       <div className="flex justify-center items-center gap-4  m-4 bg-green-200 border-2 border-green-400 rounded-2xl py-4 px-4">
-         
+        <Link to="/dashboard/home">
+          <img
+            className="w-14 "
+            src="https://cdn-icons-png.flaticon.com/128/3917/3917711.png"
+            alt=""
+          />
+        </Link>
 
-          <Link  to="/dashboard/home">
-            <img
-              className="w-14 "
-              src="https://cdn-icons-png.flaticon.com/128/3917/3917711.png"
-              alt=""
-            />
-          </Link>
-
-          <Link to="/dashboard/learning-grade">
-            <img
-              className="w-14 m-2"
-              src="https://cdn-icons-png.flaticon.com/128/4185/4185218.png"
-            />
-          </Link>
-          <Link to="/dashboard/vidéos">
-            <img
-              className="w-12 m-4"
-              src="https://cdn-icons-png.flaticon.com/128/5948/5948543.png"
-            />
-          </Link>
-
+        <Link to="/dashboard/learning-grade">
+          <img
+            className="w-14 m-2"
+            src="https://cdn-icons-png.flaticon.com/128/4185/4185218.png"
+          />
+        </Link>
+        <Link to="/dashboard/vidéos">
+          <img
+            className="w-12 m-4"
+            src="https://cdn-icons-png.flaticon.com/128/5948/5948543.png"
+          />
+        </Link>
+        <Link to="/dashboard/educational-news">
           <img
             className="w-12 m-4"
             src="https://cdn-icons-png.flaticon.com/128/3208/3208799.png"
           />
-          
-        </div>
-      </section>
-    )
-}
-export default ResponsiveSideBar
+        </Link>
+      </div>
+    </section>
+  );
+};
+export default ResponsiveSideBar;
