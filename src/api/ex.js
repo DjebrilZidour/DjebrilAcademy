9961343c4f13513d8d0724812b0edbc58e9abb78
Djebril <djebrilacademy@gmail.com>
2024-09-16 17:18:49 +0200

    fixed little bug + stating fixing the exercice style

--- src/api/ex.js
+++ src/api/ex.js
@@ -1,92 +1,288 @@
-exDataBase = [
-    {
-        module: "math",
-        semesters: [
-          [
-            {
-              courseTitle: "cours 1 de trimestre 1",
-              courseDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
-              courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800",
-              pdfUrl: "",
-            },
-            {
-              courseTitle: "course math title sem 1",
-              courseDetail: "course math detail sem 1",
-            }, {
-              courseTitle: "cours 1 de trimestre 1",
-              courseDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
-              courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800",
-              pdfUrl: "",
-            }, {
-              courseTitle: "cours 1 de trimestre 1",
-              courseDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
-              courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800",
-              pdfUrl: "",
-            }, {
-              courseTitle: "cours 1 de trimestre 1",
-              courseDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
-              courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800",
-              pdfUrl: "",
-            },
-          ],
-          [
-            {
-              courseTitle: "course math title sem 2",
-              courseDetail: "course math detail sem 2",
-              courseImage: "",
-            },
-            {
-              courseTitle: "course math title sem 2",
-              courseDetail: "course math detail sem 2",
-              courseImage: "",
-            },
-          ],
-          [
-            {
-              courseTitle: "course math title sem 3",
-              courseDetail: "course math detail sem 3",
-              courseImage: "",
-            },
-            {
-              courseTitle: "course math title sem 2",
-              courseDetail: "course math detail sem 2",
-            },
-          ],
-        ],
-      },//!MATH MODULE DATA BASE EXERCICE
-      {
-        module: "phisics",
-        semesters: [
-          [
-            {
-              courseTitle: "course phisics title sem 1",
-              courseDetail: "course phisics detail sem 1",
-            },
-            {
-              courseTitle: "course phisics title sem 1",
-              courseDetail: "course phisics detail sem 1",
-            },
-          ],
-          [
-            {
-              courseTitle: "course math title sem 2",
-              courseDetail: "course phisics detail sem 2",
-            },
-            {
-              courseTitle: "course phisics title sem 2",
-              courseDetail: "course phisics detail sem 2",
-            },
-          ],
-          [
-            {
-              courseTitle: "course phisics title sem 2",
-              courseDetail: "course phisics detail sem 2",
-            },
-            {
-              courseTitle: "course phisics title sem 2",
-              courseDetail: "course phisics detail sem 2",
-            },
-          ],
-        ],
-      }  //!PHISICS MODULE DATA BASE EXERCICE
-]
+const dataBase = [
+  {
+    module: "math",
+    semesters1: [
+      [
+        {
+          exerciceTitle: "مبادئ في الحسابيات",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "https://drive.google.com/file/d/13r7y6yv2HNITtNUuLRO8T6YvbNJ_9LRr/preview",
+          pdfId: "13r7y6yv2HNITtNUuLRO8T6YvbNJ_9LRr",
+
+        },
+        {
+          exerciceTitle: "exercice math title sem 1",
+          exerciceDetail: "exercice math detail sem 1",
+        }, {
+          exerciceTitle: "cours 1 de trimestre 1",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        }, {
+          exerciceTitle: "cours 1 de trimestre 1",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        }, {
+          exerciceTitle: "cours 1 de trimestre 1",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "cour jdid",
+          exerciceDetail: " hani creto dok he breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "cou2",
+          exerciceDetail: "exercice math detail sem 2",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice math detail sem 2",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice math title sem 3",
+          exerciceDetail: "exercice math detail sem 3",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice math detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+    ],
+    semesters2: [
+      [
+       {
+          exerciceTitle: "cour jdid fi 2eme annee trim 1",
+          exerciceDetail: " hani creto dok he breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "kolna 2eme",
+          exerciceDetail: "exercice math detail sem 2",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+        
+      ],
+      [
+        {
+          exerciceTitle: "exercice math title sem 3",
+          exerciceDetail: "exercice math detail sem 3",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice math detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+    ],
+    semesters3: [
+      [
+        {
+          exerciceTitle: " marhba f 3eme laeziz yaw andak lbaaac  ",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "https://drive.google.com/file/d/13r7y6yv2HNITtNUuLRO8T6YvbNJ_9LRr/preview",
+          pdfId: "13r7y6yv2HNITtNUuLRO8T6YvbNJ_9LRr",
+
+        },
+        {
+          exerciceTitle: "exercice math title sem 1",
+          exerciceDetail: "exercice math detail sem 1",
+        }, {
+          exerciceTitle: "cours 1 de trimestre 1",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        }, {
+          exerciceTitle: "cours 1 de trimestre 1",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        }, {
+          exerciceTitle: "cours 1 de trimestre 1",
+          exerciceDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "cour jdid",
+          exerciceDetail: " hani creto dok he breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
+          exerciceImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice math detail sem 2",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice math detail sem 2",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice math title sem 3",
+          exerciceDetail: "exercice math detail sem 3",
+          exerciceImage: "",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice math detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+    ],
+  },
+  {
+    module: "physics",
+    semesters1: [
+      [
+        {
+          exerciceTitle: "exercice phisics title sem 1",
+          exerciceDetail: "exercice phisics detail sem 1",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 1",
+          exerciceDetail: "exercice phisics detail sem 1",
+          pdfUrl: "",
+
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+    ],
+    semesters2: [
+      [
+        {
+          exerciceTitle: "exercice phisics title sem 1",
+          exerciceDetail: "exercice phisics detail sem 1",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 1",
+          exerciceDetail: "exercice phisics detail sem 1",
+          pdfUrl: "",
+
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+    ],
+    semesters3: [
+      [
+        {
+          exerciceTitle: "exercice phisics title sem 1",
+          exerciceDetail: "exercice phisics detail sem 1",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 1",
+          exerciceDetail: "exercice phisics detail sem 1",
+          pdfUrl: "",
+
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice math title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+      [
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+        {
+          exerciceTitle: "exercice phisics title sem 2",
+          exerciceDetail: "exercice phisics detail sem 2",
+          pdfUrl: "",
+        },
+      ],
+    ],
+  }
+
+
+
+];
