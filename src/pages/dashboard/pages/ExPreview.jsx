2ea56a58a20b1a791abe8d5936c77a38acdb8527
Djebril <djebrilacademy@gmail.com>
2024-09-18 11:13:59 +0200

    done exam file system

--- src/pages/dashboard/pages/ExPreview.jsx
+++ src/pages/dashboard/pages/ExPreview.jsx
@@ -1,17 +1,28 @@
 import { useLocation } from "react-router";
 import { fetchExDetail } from "../../../api/ex"
+import { useParams } from "react-router";
+const modules = [
+  {
+    moduleName: "math",
+    imgUrl:
+      "https://images.pexels.com/photos/6238050/pexels-photo-6238050.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    moduleName: "physics",
+    imgUrl:
+      "https://media.istockphoto.com/id/953006834/photo/science-math-chemistry-equations.jpg?s=612x612&w=0&k=20&c=kF7XAjZlhC1QgUOEZS7A6p6tMYAS1oF4nFLG5XQQgbE=",
+  },
+];
 const ExPreview = ()=>{
-
-const {state} = useLocation()
+  const { moduleName, semesterNumber, idx, grade } = useParams(); // Extract from URL
+  const trans = "trimester_" + semesterNumber;
+  const moduleData = modules.find((element) => element.moduleName === moduleName);
+  const examData = fetchExDetail(moduleName.toLowerCase(),
+  semesterNumber - 1,grade)
+ const exam = examData[idx]
 return(
     <>
-      <div className="flex justify-end w-full">
-        <h1 className="text-3xl uppercase background border-black border-2 px-2 py-5 cursor-pointer rounded-full "> AS</h1>
-      </div>
-    <section>
-        <h1 className="text-5xl">HERES THE EXAM PREVIEW WELCOME</h1>
-       
-    </section>
+        <h1 className="text-5xl">exam{exam.examTitle}</h1>
     </>
 )
 }
--- src/pages/dashboard/pages/TePreview.jsx
+++ src/pages/dashboard/pages/TePreview.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router";
+import { fetchTestDetail } from "../../../api/ex"
 const TePreview = ()=>{
+    const { moduleName, semesterNumber, idx, grade } = useParams(); // Extract from URL
+    const trans = "trimester_" + semesterNumber;
     return (
         <>
         <h1>gg</h1>
