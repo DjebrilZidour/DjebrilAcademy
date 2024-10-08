fb382cf9a1093c0c6dca22e862f9d5b6c99af158
Djebril <djebrilacademy@gmail.com>
2024-10-08 23:27:42 +0100

    change the landing pic

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -116,7 +116,7 @@ const Landing = ({ language }) => {
           <div className="md:w-1/2">
             <img
               className="rounded-3xl p-4 md:p-0 w-full"
-              src="https://plus.unsplash.com/premium_photo-1671069848005-7231fc25703f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHN0dWR5aW5nfGVufDB8fDB8fHww"
+              src="https://images.pexels.com/photos/2831794/pexels-photo-2831794.jpeg?auto=compress&cs=tinysrgb&w=800"
               alt=""
             />
           </div>
@@ -243,7 +243,7 @@ const Landing = ({ language }) => {
             img="https://images.pexels.com/photos/8720598/pexels-photo-8720598.jpeg?auto=compress&cs=tinysrgb&w=800"
           />
         </div>
-        <Link to="/team">
+        <Link to="/about">
           <Btn value="See more" />
         </Link>
       </section>
