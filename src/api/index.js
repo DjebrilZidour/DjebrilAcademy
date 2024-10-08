e1dee891ed1955d864ca038c33a20596cd66973f
Djebril <djebrilacademy@gmail.com>
2024-10-08 23:20:49 +0100

    start cleaning the DATA BASE

--- src/api/index.js
+++ src/api/index.js
@@ -11,25 +11,7 @@ const dataBase = [
           pdfId: "13r7y6yv2HNITtNUuLRO8T6YvbNJ_9LRr",
 
         },
-        {
-          courseTitle: "course math title sem 1",
-          courseDetail: "course math detail sem 1",
-        }, {
-          courseTitle: "cours 1 de trimestre 1",
-          courseDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
-          courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
-          pdfUrl: "",
-        }, {
-          courseTitle: "cours 1 de trimestre 1",
-          courseDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
-          courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
-          pdfUrl: "",
-        }, {
-          courseTitle: "cours 1 de trimestre 1",
-          courseDetail: "So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
-          courseImage: "https://images.pexels.com/photos/220301/pexels-photo-220301.jpeg?auto=compress&cs=tinysrgb&w=800", pdfUrl: "",
-          pdfUrl: "",
-        },
+        
         {
           courseTitle: "cour jdid",
           courseDetail: " hani creto dok he breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of all living things but I tell you Jerry at that moment, I was a marine biologist.",
